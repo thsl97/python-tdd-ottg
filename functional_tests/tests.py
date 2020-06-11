29d0049314151c3805a04c11e0def57ae8aa78b5
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2020-06-11 11:37:47 -0300

    Use Bootstrap to improve page layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,14 +2,14 @@ from selenium.webdriver.firefox.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 
 
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = self.start_new_browser_session()
