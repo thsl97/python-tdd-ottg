d70335a46c1c35e19ea0b7fc4ab6fc221d7b648e
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2020-06-18 10:27:35 -0300

    Refactor code for pep8 compliance

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -48,7 +48,9 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, 'A new list item')
 
     def test_redirects_after_POST(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        response = self.client.post('/lists/new', data={
+            'item_text': 'A new list item'
+        })
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
