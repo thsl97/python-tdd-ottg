d0a792962519e1548e911df7598ad438199c9a33
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2020-06-23 09:52:02 -0300

    Add Superlists namespace to JS

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,5 @@
-let initialize = function() {
+window.Superlists = {}
+window.Superlists.initialize = function() {
     $('input[name="text"]').on('keypress', function() {
         $('.has-error').hide()
     })
