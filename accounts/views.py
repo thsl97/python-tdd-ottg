292e3068c49ece0038ca8f41fe2472d58b249743
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2020-06-24 12:57:11 -0300

    Generating tokens from user email

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
+from django.contrib import messages
+from accounts.models import Token
+from django.urls import reverse
 
 
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
         [email]
     )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in"
+    )
+    return redirect('/')
+
+
+def login(request):
     return redirect('/')
