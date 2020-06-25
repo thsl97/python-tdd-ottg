b2a64743519baaad156976d15cb8f9d82908853c
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2020-06-25 07:26:05 -0300

    test_my_lists: precreate sessions, move login checks into base

---
+++ functional_tests/test_my_lists.py
@@ -0,0 +1,35 @@
+from .base import FunctionalTest
+from django.contrib.auth import get_user_model
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.auth import SESSION_KEY, BACKEND_SESSION_KEY
+from django.conf import settings
+
+
+User = get_user_model()
+
+
+class MyListsTest(FunctionalTest):
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        # Visit domain to create cookie, using 404 because it loads faster
+        self.browser.get(f'{self.live_server_url}/404_no_such_url/')
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/',
+        ))
+
+    def test_logged_in_user_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_out(email=email)
+
+        # Edith is a logged in user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email=email)
