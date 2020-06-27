5868be826691531f14237718b0b92fad15bbdfb9
Thalysson Henrique <thalysson-henrique_pb@hotmail.com>
2020-06-27 11:45:19 -0300

    Add screenshot on failure to FT runner

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from selenium import webdriver
 import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -10,6 +12,9 @@ from .server_tools import reset_database
 
 
 MAX_WAIT = 10
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 
 class FunctionalTest(StaticLiveServerTestCase):
@@ -22,7 +27,16 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(self.staging_server)
 
     def tearDown(self):
+        if self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
 
     def wait(fn):
         def modified_fn(*args, **kwargs):
@@ -70,3 +84,27 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.get_item_input_box().send_keys(Keys.ENTER)
         item_number = num_rows + 1
         self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
+
+    def _test_has_failed(self):
+        return any(error for (method, error) in self._outcome.errors)
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('Screenshotting to ', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('Dumping page to ', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
